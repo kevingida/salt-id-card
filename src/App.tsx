a369080c9bf6737db9643809c9c8f0358c70fa5b
kevingida <kevingida@gmail.com>
2024-05-13 15:08:52 +0200

    add error state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import { useLocation } from "react-router-dom";
-import { Main } from "./components";
+import { Main, PageNotFound } from "./components";
+import { useEffect, useState } from "react";
 
 function App() {
   const course = new URLSearchParams(useLocation().search).get("name");
   const startDate = new URLSearchParams(useLocation().search).get("date");
+  const [error, setError] = useState(false);
 
-  return (
-    <div className="root">
-      <h2>{course}</h2>
-      <h2>{startDate}</h2>
-      <Main />
-    </div>
-  );
+  useEffect(() => {
+    console.log(course);
+    if (course == null || startDate == null) {
+      setError(true);
+    }
+  }, []);
+
+  return <div className="root">{error ? <PageNotFound /> : <Main />}</div>;
 }
 
 export default App;
