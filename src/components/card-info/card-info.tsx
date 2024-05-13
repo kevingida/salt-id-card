c6f13e54a8f3cb31a557a4e73861c92f8eea7b46
kevingida <kevingida@gmail.com>
2024-05-13 16:27:59 +0200

    styling for mobile

--- src/components/card-info/card-info.tsx
+++ src/components/card-info/card-info.tsx
@@ -1,22 +1,29 @@
-import {Title} from "../title";
-import './card-info.css'
-import {ApplicationSteps} from "../application-steps";
+import "./card-info.css";
+import { ApplicationSteps } from "../application-steps";
+import Subtitle from "../subtitle/subtitle";
 
 type Props = {
-  name: string | null
-}
-export const CardInfo = ({name}: Props) => {
+  name: string | null;
+};
+export const CardInfo = ({ name }: Props) => {
   return (
     <div className="card-info__container">
-      <Title content={`Hi, ${name}!`}/>
+      <Subtitle content={`Hi, ${name}!`} />
       <main className="card-info-main">
-        <p>This card can be used to apply for Github Student Developer Pack. You can sign up for it <a
-          href="https://education.github.com/discount_requests/application" target="_blank"> here
-        </a>.
+        <p>
+          This card can be used to apply for Github Student Developer Pack. You
+          can sign up for it{" "}
+          <a
+            href="https://education.github.com/discount_requests/application"
+            target="_blank"
+          >
+            {" "}
+            here
+          </a>
+          .
         </p>
-        <ApplicationSteps/>
+        <ApplicationSteps />
       </main>
     </div>
   );
 };
-
