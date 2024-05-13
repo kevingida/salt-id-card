b08d1de7a545282e43b29095466eed62ee26fc99
kevingida <kevingida@gmail.com>
2024-05-13 14:45:04 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useLocation } from "react-router-dom";
+import { Main } from "./components";
 
 function App() {
   const course = new URLSearchParams(useLocation().search).get("name");
@@ -9,6 +10,7 @@ function App() {
     <>
       <h2>{course}</h2>
       <h2>{startDate}</h2>
+      <Main />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 const router = createBrowserRouter([
   {
@@ -13,6 +19,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>
 );
