991d8abd722d6333b76b7315d8926f5d869f1de3
kokakarova <kostadinkakarova@gmail.com>
2024-05-14 14:42:13 +0200

    add functionality to redirect button

--- src/components/page-not-found/page-not-found.tsx
+++ src/components/page-not-found/page-not-found.tsx
@@ -1,30 +1,37 @@
-import './page-not-found.css'
-import {ChangeEvent, useState} from "react";
-import {CtaButton, Form} from "../../components";
+import "./page-not-found.css";
+import { ChangeEvent, useState } from "react";
+import { CtaButton, Form } from "../../components";
+import { redirect, useNavigate } from "react-router-dom";
 
 export const PageNotFound = () => {
   const [input, setInput] = useState({
     date: "",
-    course: "jfs",
-    location: "sthlm"
+    course: "",
+    location: "sthlm",
   });
-  const [dateValid, setDateValid]= useState(false)
+  const [dateValid, setDateValid] = useState(false);
+  const redirect = useNavigate();
 
   const inputForm = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     e.preventDefault();
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setInput({
       ...input,
       [name]: value,
     });
   };
-  const {date, course, location} = input
-  const handleSubmit =()=>{
-    return null;
-  }
+  const { date, course, location } = input;
+  // const baseUrl = "http://localhost:5173/vite-react-router/";
+  const baseUrl = "https://kevingida.github.io/vite-react-router/";
+  const queryParams = `?date=${date}&name=${course}&location=${location}`;
+
+  const handleSubmit = () => {
+    redirect(queryParams);
+  };
+
   const isDateValid = (e: ChangeEvent<HTMLInputElement>) => {
-    const year = + (e.target.value.split("-")[0]);
-    if(year < 2018){
+    const year = +e.target.value.split("-")[0];
+    if (year < 2018) {
       setDateValid(false);
       return;
     }
@@ -33,10 +40,18 @@ export const PageNotFound = () => {
 
   return (
     <div className="page-not-found__container">
-      <div className="page-not-found">Page not found. Please check the url and try again.</div>
-      <Form onChange={inputForm} checkIfDateValid={isDateValid} dateValid={dateValid}/>
-      <CtaButton variant="primary" onClick={handleSubmit} disabled={!dateValid}>Redirect</CtaButton>
-      <span>{`https://localhost:5173/vite-react-router/?date=${date}&name=${course}&location=${location}`}</span>
+      <div className="page-not-found">
+        Invalid url Page not found. Please check the url and try again.
+      </div>
+      <Form
+        onChange={inputForm}
+        checkIfDateValid={isDateValid}
+        dateValid={dateValid}
+      />
+      <CtaButton variant="primary" onClick={handleSubmit} disabled={!dateValid}>
+        Redirect
+      </CtaButton>
+      <span>{baseUrl + queryParams}</span>
     </div>
   );
 };
