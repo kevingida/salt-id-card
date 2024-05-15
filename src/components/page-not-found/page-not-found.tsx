ec3125d8acf7390a26d94b0a73976bbc820a9eab
Mauro Morales <mauroale2501@gmail.com>
2024-05-15 14:56:23 +0200

    Create clipboard component and refactor page-not-found.tsx

--- src/components/page-not-found/page-not-found.tsx
+++ src/components/page-not-found/page-not-found.tsx
@@ -2,9 +2,8 @@ import "./page-not-found.css";
 import { ChangeEvent, useState } from "react";
 import { CtaButton, Form, Tooltip } from "../../components";
 import { isDisabled } from "../../utils/utils";
-import toast, { Toaster } from "react-hot-toast";
-import { ClipboardDocumentListIcon } from "@heroicons/react/24/outline";
-
+import { PageNotFoundHeader } from "./page-not-found-header";
+import { Clipboard } from "../clipboard";
 
 export const PageNotFound = () => {
   const [input, setInput] = useState({
@@ -14,7 +13,6 @@ export const PageNotFound = () => {
   });
 
   const inputForm = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    e.preventDefault();
     const { name, value } = e.target;
     setInput({
       ...input,
@@ -27,39 +25,23 @@ export const PageNotFound = () => {
   const queryParams = `?date=${date}&name=${course}&location=${location}`;
   const disabled = isDisabled(date, course, location);
 
-  const copyLink = () => {
-    navigator.clipboard
-      .writeText(baseUrl + queryParams)
-      .catch((err) => toast.error(err));
-    toast.success("Linked copied to clipboard!");
-  };
-
   return (
     <div className="page-not-found__container">
-      <div className="page-not-found">
-        <h1 className="page-not-found__title">404</h1>
-        <h2 className="page-not-found__subtitle">Page Not Found</h2>
-
-        <p>
-          Please check the url or generate one with the form below and try again
-        </p>
-      </div>
+      <PageNotFoundHeader />
       <Form onChange={inputForm} />
-      <Tooltip content="Fill all fields!" disabled={disabled} href={queryParams}>
-        <CtaButton
-          variant="primary"
-          disabled={disabled}
-        >
+      <Tooltip
+        content="Fill all fields!"
+        disabled={disabled}
+        href={queryParams}
+      >
+        <CtaButton variant="primary" disabled={disabled}>
           Redirect
         </CtaButton>
-        </Tooltip>
+      </Tooltip>
       <div className="page-not-found__url-container">
         {baseUrl + queryParams}
-          <Tooltip content="Copy to clipboard!" >
-          <ClipboardDocumentListIcon className="clipboard" onClick={copyLink} />
-          </Tooltip>
+        <Clipboard text={baseUrl + queryParams}/>
       </div>
-      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
