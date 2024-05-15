a24a1a92c36dcfaf8ab23089bde8e8dff9fceb15
kevingida <kevingida@gmail.com>
2024-05-15 09:32:09 +0200

    add 404

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../app.tsx";
-import {PageNotFound} from "../components";
+import { PageNotFound } from "../components";
 
 export const router = createBrowserRouter([
   {
-    path: "/salt-id-card/",
-    element: <App/>,
-    errorElement: <PageNotFound />
+    path: "/",
+    element: <App />,
+    errorElement: <PageNotFound />,
   },
 ]);
